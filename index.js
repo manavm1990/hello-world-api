f67bb04af5ac223f181c662cf911a6925b8dab94
manavm1990 <manavm@visionwebsoft.com>
2018-07-09 12:27:16 -0500

    Add response processing.

--- index.js
+++ index.js
@@ -7,9 +7,18 @@ const router = require('./lib/router.js');
 /**
  * Start the server, and
  * simply pass the request and response to 'router.'
+ * Process the returned spit.
  */
 const server = http.createServer((req, resp) => {
-  router.route(req, resp);
+  router.route(req, (statusCode, respPayload) => {
+    const verifiedStatusCode = typeof (statusCode) === 'number' ? statusCode : 200;
+
+    // Convert payload to string
+    const plString = JSON.stringify(respPayload);
+
+    resp.writeHead(verifiedStatusCode);
+    resp.end(plString);
+  });
 });
 
 server.listen(port, () => {
